b979e96221e5f6c9dd6d4673ec2e5223dc6dd7f1
CyberBelka <vfrosenyuk@gmail.com>
2021-07-17 11:56:19 +0400

    Второй шанс

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,9 @@ const feedbackClose = feedbackPopup.querySelector(".modal-close");
 const feedbackForm = feedbackPopup.querySelector(".feedback-form");
 const feedbackUser = feedbackPopup.querySelector(".feedback-user");
 const feedbackEmail = feedbackPopup.querySelector(".feedback-email");
+const mapLink = document.querySelector(".map");
+const mapPopup = document.querySelector(".modal-map");
+const mapClose = mapPopup.querySelector(".modal-close");
 
 let isStorageSupport = true;
 let storage = "";
@@ -35,9 +38,9 @@ feedbackClose.addEventListener("click", function(evt) {
 feedbackForm.addEventListener("submit", function(evt) {
     if (!feedbackUser.value || !feedbackEmail.value) {
         evt.preventDefault();
-        feedbackPopup.classList.add("modal-error");
-        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
         feedbackPopup.classList.remove("modal-error");
+        feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+        feedbackPopup.classList.add("modal-error");
     } else {
         if (isStorageSupport) {
             localStorage.setItem("name", feedbackUser.value);
@@ -50,6 +53,26 @@ window.addEventListener("keydown", function(evt) {
         if (feedbackPopup.classList.contains("modal-show")) {
             evt.preventDefault();
             feedbackPopup.classList.remove("modal-show");
+            feedbackPopup.classList.remove("modal-error");
+        }
+    }
+});
+
+mapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.key === "Esc" || evt.key === "Escape") {
+        if (mapPopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            mapPopup.classList.remove("modal-show");
         }
     }
 });
